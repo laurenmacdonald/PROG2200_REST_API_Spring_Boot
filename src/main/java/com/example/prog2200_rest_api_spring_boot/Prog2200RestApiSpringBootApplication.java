35d913c191f9411260adbf9920737fd931c9f864
laurenmacdonald <w0230178@nscc.ca>
2024-01-04 18:36:50 -0400

    Initial commit

---
+++ src/main/java/com/example/prog2200_rest_api_spring_boot/Prog2200RestApiSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.prog2200_rest_api_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prog2200RestApiSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Prog2200RestApiSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/prog2200_rest_api_spring_boot/Prog2200RestApiSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.prog2200_rest_api_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prog2200RestApiSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
